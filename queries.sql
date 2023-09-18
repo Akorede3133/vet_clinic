e1da2eae5c9eaaeb8fa65e74717e9c6aa5dea8f8
akorede <emiolasaheed1@gmail.com>
2023-09-18 16:40:26 +0100

    Correct date query

--- queries.sql
+++ queries.sql
@@ -2,7 +2,7 @@
 
 SELECT * from animals WHERE name LIKE '%mon'; -- Find all animals whose name ends in "mon".
 
-SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01'; -- List the name of all animals born between 2016 and 2019.
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31'; -- List the name of all animals born between 2016 and 2019.
 
 SELECT name FROM animals WHERE neutered = TRUE AND escape_attempts < 3; -- List the name of all animals that are neutered and have less than 3 escape attempts.
 

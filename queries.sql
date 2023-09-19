bb3914a5fcdb585a1c9797e6662138aac88ce99e
akorede <emiolasaheed1@gmail.com>
2023-09-19 14:30:12 +0100

    Rollback to the savepoint

--- queries.sql
+++ queries.sql
@@ -75,3 +75,6 @@ SAVEPOINT DELETEDATE2022;
 -- Update all animals' weight to be their weight multiplied by -1.
 UPDATE animals
 SET weight_kg = weight_kg * -1;
+
+-- Rollback to the savepoint
+ROLLBACK TO DELETEDATE2022;
